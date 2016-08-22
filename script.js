eb63a61b530e8ad1442dba0016c4a12aeb7dd7e2
Andrew Yaksic <nutclubflush@gmail.com>
2016-08-22 15:35:36 -0400

    final projects

---
+++ script.js
@@ -0,0 +1,30 @@
+var newGrid = 16;
+
+function drawGrid() {
+	for (i=1;i<=newGrid**2;i++) {
+		$(".container").append("<div class='square0'></div>")
+	}
+	var squareWidth = 640/newGrid;
+	$("div div").css("width",squareWidth).css("height",squareWidth);
+};
+
+function etchSketch() {
+	$("div div").mouseenter(function(){
+		var classNum = parseInt($(this).attr("class").substring(6),10);
+		var newClass = classNum+1;
+		if(newClass<11){
+			$(this).removeClass().addClass("square" + newClass);
+		};
+	});
+};
+
+$(document).ready(function(){
+	drawGrid();
+	etchSketch();
+	$(".reset").click(function(){
+		$("div div").remove();
+		newGrid = parseInt(prompt("How many squares per side?"),10);
+		drawGrid();
+		etchSketch();
+	});
+});
